48463a341c2aed101182b0b9205113bd5af0d4e8
Tyler Berry <tyler.berry4@gmail.com>
2015-01-19 09:39:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardEventApp::Application.config.secret_token = 'fd1e793120750b37714117ae1c690f3a4e574627d0ff4ce7b60d44ca752d2f278165bc9665db3ee52f2e19c8aef2370a96b9abaec15c2aaff5089d7e1c309064'
