9b94eb2d7816e68775c5d20fa484ceb764fcb2a6
Tyler Berry <tyler.berry4@gmail.com>
2015-01-19 11:34:05 -0500

    added devise for authentication, currently displays all eventbrite events for specified user

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+
+
+  def index
+    eb_client = EventbriteClient.new( {app_key: 'JLAH2BUMQXLFQBPZZY', user_key: '1421680383133097458080'})
+    events = eb_client.user_list_events["events"]
+    @html_array = get_event_html events
+    @html_array
+  end
+
+
+  protected
+
+  def get_event_html(events_array)
+    event_html_array = []
+
+    events_array.each do |event_hash|
+      event_html_array << EventbriteWidgets::ticketWidget(event_hash["event"])
+    end
+
+    event_html_array
+
+  end
+
+end
